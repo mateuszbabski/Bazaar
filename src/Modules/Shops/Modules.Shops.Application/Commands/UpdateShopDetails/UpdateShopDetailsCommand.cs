429515a989767d948f915d4592b14397ad4ca98f
mateuszbabski <babskimateusz@gmail.com>
2023-06-19 15:24:49 +0200

    Update shop module  command

---
+++ src/Modules/Shops/Modules.Shops.Application/Commands/UpdateShopDetails/UpdateShopDetailsCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace Modules.Shops.Application.Commands.UpdateShopDetails
+{
+    public class UpdateShopDetailsCommand : IRequest
+    {
+        public Guid Id { get; set; }
+        public string OwnerName { get; set; }
+        public string OwnerLastName { get; set; }
+        public string ShopName { get; set; }
+        public string TaxNumber { get; set; }
+        public string ContactNumber { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
---
+++ src/Modules/Shops/Modules.Shops.Application/Commands/UpdateShopDetails/UpdateShopDetailsCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using MediatR;
+using Modules.Shops.Domain.Repositories;
+using Shared.Abstractions.UnitOfWork;
+using Shared.Abstractions.UserServices;
+using Shared.Application.Exceptions;
+
+namespace Modules.Shops.Application.Commands.UpdateShopDetails
+{
+    public class UpdateShopDetailsCommandHandler : IRequestHandler<UpdateShopDetailsCommand>
+    {
+        private readonly ICurrentUserService _userService;
+        private readonly IShopRepository _shopRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UpdateShopDetailsCommandHandler(ICurrentUserService userService,
+                                               IShopRepository shopRepository,
+                                               IUnitOfWork unitOfWork)
+        {
+            _userService = userService;
+            _shopRepository = shopRepository;
+            _unitOfWork = unitOfWork;
+        }
+        public async Task Handle(UpdateShopDetailsCommand command, CancellationToken cancellationToken)
+        {
+            var userId = _userService.UserId;
+            var shop = await _shopRepository.GetShopById(command.Id);
+
+            if (shop == null || shop.Id.Value != userId)
+            {
+                throw new BadRequestException("Shop not found");
+            }
+
+            shop.UpdateShopDetails(command.OwnerName,
+                                   command.OwnerLastName,
+                                   command.ShopName,
+                                   command.Country,
+                                   command.City,
+                                   command.Street,
+                                   command.PostalCode,
+                                   command.TaxNumber,
+                                   command.ContactNumber);
+
+            await _unitOfWork.CommitAndDispatchEventsAsync();
+        }
+    }
+}
