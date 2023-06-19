a485f41ca4b7d63afd573ee1e8dd6a8e798d2b62
mateuszbabski <babskimateusz@gmail.com>
2023-06-19 15:40:17 +0200

    Get shop by id query test

--- src/Tests/Modules/Shops/Bazaar.Modules.Shops.Tests.Unit/Application/GetShopByIdQueryTest.cs
+++ src/Tests/Modules/Shops/Bazaar.Modules.Shops.Tests.Unit/Application/GetShopByIdQueryTest.cs
@@ -1,12 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Bazaar.Modules.Shops.Tests.Unit.Domain;
+using Modules.Shops.Application.Dtos;
+using Modules.Shops.Application.Queries.GetShopById;
+using Modules.Shops.Domain.Repositories;
+using Moq;
+using Shared.Application.Exceptions;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace Bazaar.Modules.Shops.Tests.Unit.Application
 {
-    internal class GetShopByIdQueryTest
+    public class GetShopByIdQueryTest
     {
+        private readonly GetShopByIdQueryHandler _sut;
+        private readonly Mock<IShopRepository> _shopRepositoryMock = new(); 
+        public GetShopByIdQueryTest()
+        {
+            _sut = new GetShopByIdQueryHandler(_shopRepositoryMock.Object);
+        }
+
+        [Fact]
+        public async Task GetShopById_ValidId_ReturnsShopDetailsDto()
+        {
+            var shop = ShopFactory.GetShop();
+
+            var query = new GetShopByIdQuery()
+            {
+                Id = shop.Id
+            };
+
+            _shopRepositoryMock.Setup(x => x.GetShopById(query.Id)).ReturnsAsync(shop);
+
+            var result = await _sut.Handle(query, CancellationToken.None);
+
+            Assert.IsType<ShopDetailsDto>(result);
+            Assert.True(shop.Id.Value == result.ShopId);
+        }
+
+        [Fact]
+        public async Task GetShopById_InvalidId_ThrowsNotFoundException()
+        {
+            var shop = ShopFactory.GetShop();
+
+            var query = new GetShopByIdQuery()
+            {
+                Id = Guid.NewGuid()
+            };
+
+            _shopRepositoryMock.Setup(x => x.GetShopById(query.Id)).ThrowsAsync(new NotFoundException("Shop not found."));
+
+            var result = await Assert.ThrowsAsync<NotFoundException>(() => _sut.Handle(query, CancellationToken.None));
+
+            Assert.IsType<NotFoundException>(result);
+            Assert.Equal("Shop not found.", result.Message);
+        }
     }
 }
