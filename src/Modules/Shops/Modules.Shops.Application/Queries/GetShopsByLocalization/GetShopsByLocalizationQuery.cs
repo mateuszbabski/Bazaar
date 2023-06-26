27b17aa6c3c8de18c8d584e5c7f39d4b506ca726
mateuszbabski <babskimateusz@gmail.com>
2023-06-26 11:26:05 +0200

    Get shops by localization query handler

--- src/Modules/Shops/Modules.Shops.Application/Queries/GetShopsByLocalization/GetShopsByLocalizationQuery.cs
+++ src/Modules/Shops/Modules.Shops.Application/Queries/GetShopsByLocalization/GetShopsByLocalizationQuery.cs
@@ -1,8 +1,11 @@
-﻿//using MediatR;
+﻿using MediatR;
+using Modules.Shops.Application.Dtos;
 
-//namespace Modules.Shops.Application.Queries.GetShopsByLocalization
-//{
-//    public class GetShopsByLocalizationQuery : IRequest<>
-//    {
-//    }
-//}
+namespace Modules.Shops.Application.Queries.GetShopsByLocalization
+{
+    public class GetShopsByLocalizationQuery : IRequest<IEnumerable<ShopDto>>
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+    }
+}
--- src/Modules/Shops/Modules.Shops.Application/Queries/GetShopsByLocalization/GetShopsByLocalizationQueryHandler.cs
+++ src/Modules/Shops/Modules.Shops.Application/Queries/GetShopsByLocalization/GetShopsByLocalizationQueryHandler.cs
@@ -1,8 +1,26 @@
-﻿//using MediatR;
+﻿using MediatR;
+using Modules.Shops.Application.Dtos;
+using Modules.Shops.Domain.Repositories;
+using Shared.Application.Exceptions;
 
-//namespace Modules.Shops.Application.Queries.GetShopsByLocalization
-//{
-//    public class GetShopsByLocalizationQueryHandler : IRequestHandler<GetShopsByLocalizationQuery, >
-//    {
-//    }
-//}
+namespace Modules.Shops.Application.Queries.GetShopsByLocalization
+{
+    public class GetShopsByLocalizationQueryHandler : IRequestHandler<GetShopsByLocalizationQuery, IEnumerable<ShopDto>>
+    {
+        private readonly IShopRepository _shopRepository;
+
+        public GetShopsByLocalizationQueryHandler(IShopRepository shopRepository)
+        {
+            _shopRepository = shopRepository;
+        }
+        public async Task<IEnumerable<ShopDto>> Handle(GetShopsByLocalizationQuery request, CancellationToken cancellationToken)
+        {
+            var shops = await _shopRepository.GetShopsByLocalization(request.Country, request.City)
+                ?? throw new NotFoundException("Shops not found");
+
+            var shopListDto = ShopDto.CreateDtoFromObject(shops);
+
+            return shopListDto;
+        }
+    }
+}
