5c70aebe501d2bcdea3af69bd59f5c892f6cf67b
mateuszbabski <babskimateusz@gmail.com>
2023-06-26 11:11:11 +0200

    Query handler get shops by name

--- src/Modules/Shops/Modules.Shops.Application/Queries/GetShopsByName/GetShopsByNameQuery.cs
+++ src/Modules/Shops/Modules.Shops.Application/Queries/GetShopsByName/GetShopsByNameQuery.cs
@@ -1,8 +1,10 @@
-﻿//using MediatR;
+﻿using MediatR;
+using Modules.Shops.Application.Dtos;
 
-//namespace Modules.Shops.Application.Queries.GetShopsByName
-//{
-//    public class GetShopsByNameQuery : IRequest<>
-//    {
-//    }
-//}
+namespace Modules.Shops.Application.Queries.GetShopsByName
+{
+    public class GetShopsByNameQuery : IRequest<IEnumerable<ShopDto>>
+    {
+        public string ShopName { get; set; }
+    }
+}
--- src/Modules/Shops/Modules.Shops.Application/Queries/GetShopsByName/GetShopsByNameQueryHandler.cs
+++ src/Modules/Shops/Modules.Shops.Application/Queries/GetShopsByName/GetShopsByNameQueryHandler.cs
@@ -1,8 +1,26 @@
-﻿//using MediatR;
+﻿using MediatR;
+using Modules.Shops.Application.Dtos;
+using Modules.Shops.Domain.Repositories;
+using Shared.Application.Exceptions;
 
-//namespace Modules.Shops.Application.Queries.GetShopsByName
-//{
-//    public class GetShopsByNameQueryHandler : IRequestHandler<GetShopsByNameQuery, >
-//    {
-//    }
-//}
+namespace Modules.Shops.Application.Queries.GetShopsByName
+{
+    public class GetShopsByNameQueryHandler : IRequestHandler<GetShopsByNameQuery, IEnumerable<ShopDto>>
+    {
+        private readonly IShopRepository _shopRepository;
+
+        public GetShopsByNameQueryHandler(IShopRepository shopRepository)
+        {
+            _shopRepository = shopRepository;
+        }
+        public async Task<IEnumerable<ShopDto>> Handle(GetShopsByNameQuery request, CancellationToken cancellationToken)
+        {
+            var shops = await _shopRepository.GetShopsByName(request.ShopName)
+                ?? throw new NotFoundException("Shops not found");
+
+            var shopListDto = ShopDto.CreateDtoFromObject(shops);
+
+            return shopListDto;
+        }
+    }
+}
