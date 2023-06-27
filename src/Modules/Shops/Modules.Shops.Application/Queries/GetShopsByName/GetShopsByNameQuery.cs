222e6667b5e23923cbc19cbbb4df8ee3b90b3632
mateuszbabski <babskimateusz@gmail.com>
2023-06-27 14:27:50 +0200

    pagination shoplist by name

--- src/Modules/Shops/Modules.Shops.Application/Queries/GetShopsByName/GetShopsByNameQuery.cs
+++ src/Modules/Shops/Modules.Shops.Application/Queries/GetShopsByName/GetShopsByNameQuery.cs
@@ -1,11 +1,14 @@
 ﻿using MediatR;
 using Modules.Shops.Application.Dtos;
+using Shared.Application.Queries;
 
 namespace Modules.Shops.Application.Queries.GetShopsByName
 {
-    public class GetShopsByNameQuery : IRequest<IEnumerable<ShopDto>>
+    public class GetShopsByNameQuery : IRequest<PagedList<ShopDto>>
     {
         #nullable enable
         public string? ShopName { get; set; }
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
     }
 }
--- src/Modules/Shops/Modules.Shops.Application/Queries/GetShopsByName/GetShopsByNameQueryHandler.cs
+++ src/Modules/Shops/Modules.Shops.Application/Queries/GetShopsByName/GetShopsByNameQueryHandler.cs
@@ -2,10 +2,11 @@
 using Modules.Shops.Application.Dtos;
 using Modules.Shops.Domain.Repositories;
 using Shared.Application.Exceptions;
+using Shared.Application.Queries;
 
 namespace Modules.Shops.Application.Queries.GetShopsByName
 {
-    public class GetShopsByNameQueryHandler : IRequestHandler<GetShopsByNameQuery, IEnumerable<ShopDto>>
+    public class GetShopsByNameQueryHandler : IRequestHandler<GetShopsByNameQuery, PagedList<ShopDto>>
     {
         private readonly IShopRepository _shopRepository;
 
@@ -13,14 +14,23 @@ namespace Modules.Shops.Application.Queries.GetShopsByName
         {
             _shopRepository = shopRepository;
         }
-        public async Task<IEnumerable<ShopDto>> Handle(GetShopsByNameQuery request, CancellationToken cancellationToken)
+        public async Task<PagedList<ShopDto>> Handle(GetShopsByNameQuery query, CancellationToken cancellationToken)
         {
-            var shops = await _shopRepository.GetShopsByName(request.ShopName)
+            var shops = await _shopRepository.GetShopsByName(query.ShopName)
                 ?? throw new NotFoundException("Shops not found");
 
-            var shopListDto = ShopDto.CreateDtoFromObject(shops);
+            var pagedShops = shops.Skip((query.PageNumber - 1) * query.PageSize)
+                                  .Take(query.PageSize)
+                                  .ToList();
 
-            return shopListDto;
+            var shopListDto = ShopDto.CreateDtoFromObject(pagedShops);
+
+            var pagedShopList = new PagedList<ShopDto>(shopListDto,
+                                                       shopListDto.Count(),
+                                                       query.PageNumber,
+                                                       query.PageSize);
+
+            return pagedShopList;
         }
     }
 }
