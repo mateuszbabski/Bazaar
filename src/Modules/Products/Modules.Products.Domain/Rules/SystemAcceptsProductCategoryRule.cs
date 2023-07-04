37ade3e69166f98245fac3ac3b5e03466be91f87
mateuszbabski <babskimateusz@gmail.com>
2023-07-04 16:31:25 +0200

    Product layer domain business rule

---
+++ src/Modules/Products/Modules.Products.Domain/Rules/SystemAcceptsProductCategoryRule.cs
@@ -0,0 +1,16 @@
+﻿using Modules.Products.Domain.ValueObjects;
+using Shared.Domain;
+
+namespace Modules.Products.Domain.Rules
+{
+    public class SystemAcceptsProductCategoryRule : Categories, IBusinessRule
+    {
+        private readonly string _categoryName;
+        public SystemAcceptsProductCategoryRule(string categoryName)
+        {
+            _categoryName = categoryName;
+        }
+        public string Message => "Category is not accepted by the system";
+        public bool IsBroken() => !CategoryList.Contains(_categoryName, StringComparer.OrdinalIgnoreCase);
+    }
+}
