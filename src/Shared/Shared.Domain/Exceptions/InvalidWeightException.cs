831938f89bdfdefbf82130f17f4c2c5ef3414ac4
mateuszbabski <babskimateusz@gmail.com>
2023-07-04 16:01:42 +0200

    Shared Domain Weight value object

---
+++ src/Shared/Shared.Domain/Exceptions/InvalidWeightException.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.Domain.Exceptions
+{
+    public class InvalidWeightException : Exception
+    {
+        public InvalidWeightException() : base(message: "Weight can't be equal or lower than 0.")
+        {
+            
+        }
+    }
+}
---
+++ src/Shared/Shared.Domain/ValueObjects/Weight.cs
@@ -0,0 +1,22 @@
+﻿using Shared.Domain.Exceptions;
+
+namespace Shared.Domain.ValueObjects
+{
+    public record Weight
+    {
+        public decimal Value { get; }
+
+        public Weight(decimal value)
+        {
+            if (value <= 0 || string.IsNullOrEmpty(value.ToString()))
+            {
+                throw new InvalidWeightException();
+            }
+
+            Value = value;
+        }
+
+        public static implicit operator decimal(Weight weight) => weight.Value;
+        public static implicit operator Weight(decimal value) => new(value);
+    }
+}
