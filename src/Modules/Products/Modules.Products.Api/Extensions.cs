bc691fb507f05f80e75b6c8100f49dfb06041672
mateuszbabski <babskimateusz@gmail.com>
2023-07-06 21:44:53 +0200

    Product module extensions added

--- src/Modules/Products/Modules.Products.Api/Extensions.cs
+++ src/Modules/Products/Modules.Products.Api/Extensions.cs
@@ -1,11 +1,15 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Modules.Products.Infrastructure;
 
 namespace Modules.Products.Api
 {
     public static class Extensions
     {
-        public static IServiceCollection AddProductsModule(this IServiceCollection services) 
+        public static IServiceCollection AddProductsModule(this IServiceCollection services, IConfiguration configuration) 
         {
+            services.AddProductsInfrastructure(configuration);
+
             return services;
         }
     }
--- src/Modules/Products/Modules.Products.Infrastructure/Extensions.cs
+++ src/Modules/Products/Modules.Products.Infrastructure/Extensions.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Modules.Products.Domain.Repositories;
+using Modules.Products.Infrastructure.Context;
+using Modules.Products.Infrastructure.DomainEvents;
+using Modules.Products.Infrastructure.Repository;
+using Shared.Infrastructure.DomainEvents;
+using Shared.Infrastructure.UnitOfWork;
 
 namespace Modules.Products.Infrastructure
 {
     public static class Extensions
     {
-        public static IServiceCollection AddProductsInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddProductsInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddDbContext<ProductsDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddDomainEventsAccessor<ProductsDomainEventsAccessor>();
+            services.AddUnitOfWork<ProductsUnitOfWork>();
+
             return services;
         }
     }
