7aeb215dc3d9bede23d9fc5c9c774583bb855bf2
mateuszbabski <babskimateusz@gmail.com>
2023-07-06 21:44:21 +0200

    Products DOmain event accessor

---
+++ src/Modules/Products/Modules.Products.Infrastructure/DomainEvents/ProductsDomainEventsAccessor.cs
@@ -0,0 +1,13 @@
+﻿using Modules.Products.Infrastructure.Context;
+using Shared.Infrastructure.DomainEvents;
+
+namespace Modules.Products.Infrastructure.DomainEvents
+{
+    internal class ProductsDomainEventsAccessor : DomainEventsAccessor<ProductsDbContext>
+    {
+        public ProductsDomainEventsAccessor(ProductsDbContext dbContext)
+            : base(dbContext)
+        {
+        }
+    }
+}
