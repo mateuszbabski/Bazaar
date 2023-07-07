b55d107eaa549d28f08ac438ddfe24e0129d52c2
mateuszbabski <babskimateusz@gmail.com>
2023-07-07 08:08:35 +0200

    Shop factory + product list mock

---
+++ src/Tests/Modules/Products/Bazaar.Modules.Products.Tests.Unit/Application/ProductListMock.cs
@@ -0,0 +1,41 @@
+﻿using Modules.Products.Domain.Entities;
+using Modules.Products.Domain.ValueObjects;
+using Modules.Shops.Domain.Entities;
+using Shared.Domain.ValueObjects;
+
+namespace Bazaar.Modules.Products.Tests.Unit.Application
+{
+    public class ProductListMock
+    {
+        private readonly List<Product> _productList;
+        public List<Product> Products
+        {
+            get { return _productList; }
+        }
+
+        public ProductListMock(Shop shop)
+        {
+            var product = Product.CreateProduct("productName",
+                                                "productDescription",
+                                                ProductCategory.Create("Food"),
+                                                1,
+                                                MoneyValue.Of(10, "USD"),
+                                                "pieces",
+                                                new ProductShopId(shop.Id));
+
+            var product2 = Product.CreateProduct("productName2",
+                                                 "productDescription2",
+                                                 ProductCategory.Create("Food"),
+                                                 1,
+                                                 MoneyValue.Of(20, "USD"),
+                                                 "pieces",
+                                                 new ProductShopId(shop.Id));
+
+            _productList = new List<Product>
+            {
+                product,
+                product2
+            };
+        }        
+    }
+}
---
+++ src/Tests/Modules/Products/Bazaar.Modules.Products.Tests.Unit/Application/ShopFactory.cs
@@ -0,0 +1,24 @@
+﻿using Modules.Shops.Domain.Entities;
+using Shared.Domain.ValueObjects;
+
+namespace Bazaar.Modules.Products.Tests.Unit.Application
+{
+    public class ShopFactory
+    {
+        public static Shop GetShop()
+        {
+            var address = Address.CreateAddress("country", "city", "street", "postalCode");
+
+            var shop = Shop.Create("shop@example.com",
+                                   "passwordHash",
+                                   "ownerName",
+                                   "ownerLastName",
+                                   "shopName",
+                                   address,
+                                   "taxNumber",
+                                   "telephoneNumber");
+
+            return shop;
+        }
+    }
+}
