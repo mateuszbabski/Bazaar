6ffd5e45f3523a3e2f53682421c1c2872c26c5c6
mateuszbabski <babskimateusz@gmail.com>
2023-07-07 09:21:53 +0200

    Product Dtos

--- src/Modules/Products/Modules.Products.Application/Dtos/ProductDetailsDto.cs
+++ src/Modules/Products/Modules.Products.Application/Dtos/ProductDetailsDto.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Modules.Products.Domain.Entities;
 
 namespace Modules.Products.Application.Dtos
 {
-    internal class ProductDetailsDto
+    public record ProductDetailsDto
     {
+        public Guid Id { get; init; }
+        public string ProductName { get;  init; }
+        public string ProductDescription { get; init; }
+        public string ProductCategory { get; init; }
+        public decimal ProductPrice { get; init; }
+        public string ProductPriceCurrency { get; init; }
+        public decimal WeightPerUnit { get; init; }
+        public string Unit { get; init; }
+        public bool IsAvailable { get; init; }
+
+        internal static ProductDetailsDto CreateDtoFromObject(Product product)
+        {
+            return new ProductDetailsDto()
+            {
+                Id = product.Id,
+                ProductName = product.ProductName,
+                ProductDescription = product.ProductDescription,
+                ProductCategory = product.ProductCategory.CategoryName,
+                ProductPrice = product.Price.Amount,
+                ProductPriceCurrency = product.Price.Currency,
+                WeightPerUnit = product.WeightPerUnit,
+                Unit = product.Unit,
+                IsAvailable = product.IsAvailable,
+            };
+        }
     }
 }
--- src/Modules/Products/Modules.Products.Application/Dtos/ProductDto.cs
+++ src/Modules/Products/Modules.Products.Application/Dtos/ProductDto.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Modules.Products.Domain.Entities;
 
 namespace Modules.Products.Application.Dtos
 {
-    internal class ProductDto
+    public record ProductDto
     {
+        public Guid Id { get; init; }
+        public string ProductName { get; init; }
+        public string ProductCategory { get; init; }
+        public decimal Price { get; init; }
+        public string Currency { get; init; }
+
+        internal static IEnumerable<ProductDto> CreateDtoFromObject(List<Product> products)
+        {
+            var productList = new List<ProductDto>();
+
+            foreach (var product in products)
+            {
+                var productDto = new ProductDto()
+                {
+                    Id = product.Id,
+                    ProductName = product.ProductName,
+                    ProductCategory = product.ProductCategory.CategoryName,
+                    Price = product.Price.Amount,
+                    Currency = product.Price.Currency
+                };
+
+                productList.Add(productDto);
+            }
+
+            return productList;
+        }
     }
 }
