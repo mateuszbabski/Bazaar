a04e037d881789344b7e84b8bb0633c1b8ec011e
mateuszbabski <babskimateusz@gmail.com>
2023-07-14 18:23:48 +0200

    Transaction failed exception

---
+++ src/Shared/Shared.Application/Exceptions/TransactionFailedException.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace Shared.Application.Exceptions
+{
+    public class TransactionFailedException : CustomException
+    {
+        public TransactionFailedException(string message)
+            : base(message, null, HttpStatusCode.BadRequest)
+        {
+            
+        }
+    }
+}
