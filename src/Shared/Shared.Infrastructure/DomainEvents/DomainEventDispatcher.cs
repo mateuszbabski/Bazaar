22876513f4ea784ca0dfb2b9728ce589b2c7b295
mateuszbabski <babskimateusz@gmail.com>
2023-07-14 18:23:13 +0200

    Accessor deleted from dispatcher

--- src/Shared/Shared.Infrastructure/DomainEvents/DomainEventDispatcher.cs
+++ src/Shared/Shared.Infrastructure/DomainEvents/DomainEventDispatcher.cs
@@ -7,13 +7,10 @@ namespace Shared.Infrastructure.DomainEvents
 {
     public sealed class DomainEventDispatcher : IDomainEventDispatcher
     {
-        private readonly IDomainEventsAccessor _domainEventsAccessor;
         private readonly IMediator _mediator;
 
-        public DomainEventDispatcher(IDomainEventsAccessor domainEventsAccessor,
-                                     IMediator mediator)
+        public DomainEventDispatcher(IMediator mediator)
         {
-            _domainEventsAccessor = domainEventsAccessor;
             _mediator = mediator;
         }
 
@@ -32,24 +29,6 @@ namespace Shared.Infrastructure.DomainEvents
                 Log.Information("Domain event: {@event}", domainEvent);
                 await _mediator.Publish(domainEvent, cancellationToken);
             }
-        }
-
-        public async Task DispatchDomainEventsAsync(CancellationToken cancellationToken = default)
-        {
-            var domainEvents = _domainEventsAccessor.GetAllDomainEvents();
-
-            if (domainEvents is null || !domainEvents.Any())
-                return;
-
-            _domainEventsAccessor.ClearAllDomainEvents();
-
-            Log.Information("Domain event: {@event}", domainEvents);
-
-            foreach (var domainEvent in domainEvents)
-            {
-                Log.Information("Domain event: {@event}", domainEvent);
-                await ((dynamic)_mediator).Publish((dynamic)domainEvent, cancellationToken);
-            }        
-        }        
+        }       
     }
 }
