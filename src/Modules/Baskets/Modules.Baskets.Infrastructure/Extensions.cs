193461d82fe0c99857b0ae738fc3a013dc04ba8f
mateuszbabski <babskimateusz@gmail.com>
2023-07-16 19:46:24 +0200

    Baskets infrastructure layer

---
+++ src/Modules/Baskets/Modules.Baskets.Infrastructure/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Modules.Baskets.Domain.Repositories;
+using Modules.Baskets.Infrastructure.Context;
+using Modules.Baskets.Infrastructure.Repository;
+using Shared.Infrastructure.UnitOfWork;
+
+namespace Modules.Baskets.Infrastructure
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddBasketsInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<BasketsDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddScoped<IBasketRepository, BasketRepository>();
+            services.AddUnitOfWork<BasketsUnitOfWork>();
+
+            return services;
+        }
+    }
+}
---
+++ src/Modules/Baskets/Modules.Baskets.Infrastructure/Repository/BasketRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Modules.Baskets.Domain.Entities;
+using Modules.Baskets.Domain.Repositories;
+using Modules.Baskets.Domain.ValueObjects;
+using Modules.Baskets.Infrastructure.Context;
+
+namespace Modules.Baskets.Infrastructure.Repository
+{
+    internal sealed class BasketRepository : IBasketRepository
+    {
+        private readonly BasketsDbContext _dbContext;
+
+        public BasketRepository(BasketsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<Basket> CreateBasket(Basket basket)
+        {
+            await _dbContext.Baskets.AddAsync(basket);
+
+            return basket;
+        }
+
+        public void DeleteBasket(Basket basket)
+        {
+            _dbContext.Baskets.Remove(basket);
+        }
+
+        public async Task<Basket> GetBasketByCustomerId(BasketCustomerId customerId)
+        {
+            return await _dbContext.Baskets.Include(x => x.Items)
+                                           .FirstOrDefaultAsync(x => x.CustomerId == customerId);
+        }
+
+        public async Task RemoveItem(Basket basket, BasketItemId basketItemId)
+        {
+            var product = await _dbContext.BasketItems.Where(x => x.BasketId == basket.Id)
+                                                      .FirstOrDefaultAsync(x => x.Id == basketItemId);
+
+            _dbContext.BasketItems.Remove(product);
+
+            if (basket.Items.Count == 0)
+            {
+                _dbContext.Baskets.Remove(basket);
+            }
+        }
+    }
+}
