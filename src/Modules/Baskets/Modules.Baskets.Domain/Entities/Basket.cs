418246d29705091016433a7d87e4a5fd5f624597
mateuszbabski <babskimateusz@gmail.com>
2023-07-16 19:01:21 +0200

    Basket module entities

--- src/Modules/Baskets/Modules.Baskets.Domain/Entities/Basket.cs
+++ src/Modules/Baskets/Modules.Baskets.Domain/Entities/Basket.cs
@@ -1,12 +1,94 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Shared.Domain.ValueObjects;
+using Shared.Domain;
+using Modules.Baskets.Domain.Events;
+using Modules.Baskets.Domain.ValueObjects;
 
 namespace Modules.Baskets.Domain.Entities
 {
-    internal class Basket
+    public class Basket : Entity, IAggregateRoot
     {
+        public BasketId Id { get; private set; }
+        public BasketCustomerId CustomerId { get; private set; }
+        public List<BasketItem> Items { get; private set; }
+        public MoneyValue TotalPrice { get; private set; }
+
+        private Basket() { }
+
+        private Basket(BasketCustomerId customerId, string currency)
+        {
+            Id = new BasketId(Guid.NewGuid());
+            CustomerId = customerId;
+            Items = new List<BasketItem>();
+            TotalPrice = new MoneyValue(0, currency);
+        }
+
+        public static Basket CreateBasket(BasketCustomerId customerId, string currency)
+        {
+            var basket = new Basket(customerId, currency);
+            basket.AddDomainEvent(new BasketCreatedDomainEvent(basket));
+
+            return basket;
+        }
+
+        private MoneyValue CountTotalPrice(List<BasketItem> items)
+        {
+            decimal allProductsPrice = items.Sum(x => x.Price.Amount);
+
+            return new MoneyValue(allProductsPrice, TotalPrice.Currency);
+        }
+
+        internal MoneyValue GetPrice()
+        {
+            return TotalPrice;
+        }
+
+        public void ChangeShoppingCartCurrency(decimal conversionRate, string currency)
+        {
+            foreach (var item in Items)
+            {
+                item.ChangeCurrency(conversionRate, currency);
+            }
+
+            decimal convertedProductsPrice = Items.Sum(x => x.Price.Amount);
+
+            this.TotalPrice = MoneyValue.Of(convertedProductsPrice, currency);
+
+            AddDomainEvent(new BasketCurrencyChangedDomainEvent(this));
+        }
+
+        public void AddProductToShoppingCart(Product product, int quantity, decimal convertedPrice)
+        {
+            var basketItem = Items.FirstOrDefault(x => x.ProductId == product.Id);
+
+            if (basketItem == null)
+            {
+                var newBasketItem = BasketItem.CreateBasketItemFromProduct(product,
+                                                                           this.Id,
+                                                                           quantity,
+                                                                           this.TotalPrice.Currency,
+                                                                           convertedPrice);
+                Items.Add(newBasketItem);
+
+                this.AddDomainEvent(new ProductAddedToBasketDomainEvent(this, newBasketItem));
+            }
+            else
+            {
+                basketItem.ChangeCartItemQuantity(quantity, convertedPrice, this.TotalPrice.Currency);
+                this.AddDomainEvent(new ProductQuantityChangedDomainEvent(this, basketItem));
+            }
+
+            this.TotalPrice = CountTotalPrice(this.Items);
+        }
+
+        public void RemoveItemFromCart(BasketItemId basketItemId)
+        {
+            var item = Items.FirstOrDefault(x => x.Id == basketItemId);
+
+            Items.Remove(item);
+
+            this.AddDomainEvent(new ProductRemovedFromBasketDomainEvent(this, item));
+
+            this.TotalPrice = CountTotalPrice(this.Items);
+        }
     }
 }
--- src/Modules/Baskets/Modules.Baskets.Domain/Entities/BasketItem.cs
+++ src/Modules/Baskets/Modules.Baskets.Domain/Entities/BasketItem.cs
@@ -1,12 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Modules.Baskets.Domain.Exceptions;
+using Modules.Baskets.Domain.ValueObjects;
+using Shared.Domain;
+using Shared.Domain.ValueObjects;
 
 namespace Modules.Baskets.Domain.Entities
 {
-    internal class BasketItem
+    public class BasketItem : Entity
     {
+        public BasketItemId Id { get; private set; }
+        public BasketProductId ProductId { get; private set; }
+        public BasketShopId ShopId { get; private set; }
+        public BasketId BasketId { get; private set; }
+        public int Quantity { get; private set; } = 1;
+        public MoneyValue Price { get; private set; }
+        public MoneyValue BaseCurrencyPrice { get; private set; }
+
+        private BasketItem() { }
+
+        // get product / projection
+        private BasketItem(Product product,
+                           BasketId basketId,
+                           int quantity,
+                           string currency,
+                           decimal convertedPrice)
+        {
+            Id = new BasketItemId(Guid.NewGuid());
+            ProductId = product.Id;
+            BasketId = basketId;
+            ShopId = product.ShopId;
+            Quantity = quantity;
+            BaseCurrencyPrice = CountCartItemPrice(quantity, product.Price.Amount, product.Price.Currency);
+            Price = CountCartItemPrice(quantity, convertedPrice, currency);
+        }
+
+        internal static BasketItem CreateBasketItemFromProduct(Product product,
+                                                               BasketId basketId,
+                                                               int quantity,
+                                                               string currency,
+                                                               decimal convertedPrice)
+        {
+            if (quantity <= 0)
+            {
+                throw new InvalidQuantityException();
+            }
+
+            return new BasketItem(product, basketId, quantity, currency, convertedPrice);
+        }
+
+        private static MoneyValue CountCartItemPrice(int quantity, decimal price, string currency)
+        {
+            return MoneyValue.Of(quantity * price, currency);
+        }
+
+        internal void ChangeCartItemQuantity(int quantity, decimal price, string currency)
+        {
+            if (quantity <= 0)
+            {
+                throw new InvalidQuantityException();
+            }
+
+            this.Quantity = quantity;
+
+            this.Price = CountCartItemPrice(quantity, price, currency);
+        }
+
+        internal void ChangeCurrency(decimal conversionRate, string currency)
+        {
+            if (this.BaseCurrencyPrice.Currency != currency)
+            {
+                this.Price = MoneyValue.Of(this.Price.Amount * conversionRate,
+                                           currency);
+            }
+            else
+            {
+                this.Price = this.BaseCurrencyPrice;
+            }
+        }
     }
 }
