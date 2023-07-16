2f17aa2ecb40dfb8d900febf7befee31337830bb
mateuszbabski <babskimateusz@gmail.com>
2023-07-16 19:01:01 +0200

    Basket module irepository

--- src/Modules/Baskets/Modules.Baskets.Domain/Repositories/IBasketRepository.cs
+++ src/Modules/Baskets/Modules.Baskets.Domain/Repositories/IBasketRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace Modules.Baskets.Domain.Repositories
+﻿using Modules.Baskets.Domain.Entities;
+using Modules.Baskets.Domain.ValueObjects;
+
+namespace Modules.Baskets.Domain.Repositories
 {
     public interface IBasketRepository
     {
+        Task<Basket> CreateBasket(Basket basket);
+        Task<Basket> GetBasketByCustomerId(BasketCustomerId customerId);
+        Task RemoveItem(Basket basket, BasketItemId basketItemId);
+        void DeleteBasket(Basket basket);
     }
 }
