867f6df4b130eb4466787a87eb50f80f8954198b
mateuszbabski <babskimateusz@gmail.com>
2023-07-16 19:46:39 +0200

    Baskets application layer dtos

---
+++ src/Modules/Baskets/Modules.Baskets.Application/Dtos/BasketDto.cs
@@ -0,0 +1,33 @@
+﻿using Modules.Baskets.Domain.Entities;
+using Shared.Domain.ValueObjects;
+
+namespace Modules.Baskets.Application.Dtos
+{
+    public class BasketDto
+    {
+        public Guid Id { get; init; }
+        public Guid CustomerId { get; init; }
+        public List<BasketItemDto> Items { get; init; }
+        public MoneyValue TotalPrice { get; init; }
+
+        public static BasketDto CreateBasketDtoFromObject(Basket basket)
+        {
+            var basketItemsList = new List<BasketItemDto>();
+
+            foreach (var item in basket.Items)
+            {
+                var basketItemDto = BasketItemDto.CreateBasketItemDtoFromObject(item);
+
+                basketItemsList.Add(basketItemDto);
+            }
+
+            return new BasketDto()
+            {
+                Id = basket.Id,
+                CustomerId = basket.CustomerId,
+                TotalPrice = basket.TotalPrice,
+                Items = basketItemsList
+            };
+        }
+    }
+}
---
+++ src/Modules/Baskets/Modules.Baskets.Application/Dtos/BasketItemDto.cs
@@ -0,0 +1,24 @@
+﻿using Modules.Baskets.Domain.Entities;
+using Shared.Domain.ValueObjects;
+
+namespace Modules.Baskets.Application.Dtos
+{
+    public class BasketItemDto
+    {
+        public Guid Id { get; init; }
+        public Guid ProductId { get; init; }
+        public int Quantity { get; init; }
+        public MoneyValue Price { get; init; }
+
+        public static BasketItemDto CreateBasketItemDtoFromObject(BasketItem basketItem)
+        {
+            return new BasketItemDto()
+            {
+                Id = basketItem.Id,
+                ProductId = basketItem.ProductId,
+                Quantity = basketItem.Quantity,
+                Price = basketItem.Price,
+            };
+        }
+    }
+}
