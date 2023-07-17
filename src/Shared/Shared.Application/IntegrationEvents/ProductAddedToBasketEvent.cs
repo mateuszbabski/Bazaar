420b80ac46052aab997bea2fccb32ff2fcb4f665
mateuszbabski <babskimateusz@gmail.com>
2023-07-17 06:47:12 +0200

    Shared.Application first integration event

---
+++ src/Shared/Shared.Application/IntegrationEvents/ProductAddedToBasketEvent.cs
@@ -0,0 +1,13 @@
+﻿using Shared.Abstractions.Events;
+using Shared.Domain.ValueObjects;
+
+namespace Modules.Shared.Application.IntegrationEvents
+{
+    public record ProductAddedToBasketEvent(Guid ProductId,
+                                            Guid ShopId,
+                                            Guid CustomerId,
+                                            MoneyValue ProductPrice,
+                                            int Quantity) : IEvent
+    {
+    }
+}
