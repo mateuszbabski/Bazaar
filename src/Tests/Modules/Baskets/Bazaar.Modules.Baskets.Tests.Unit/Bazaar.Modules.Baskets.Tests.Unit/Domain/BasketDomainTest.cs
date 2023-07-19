fb5020177b9b8ddfe0a722eb19c3be04a8adf2b0
mateuszbabski <babskimateusz@gmail.com>
2023-07-19 22:34:54 +0200

    Basket domain test

--- src/Tests/Modules/Baskets/Bazaar.Modules.Baskets.Tests.Unit/Bazaar.Modules.Baskets.Tests.Unit/Domain/BasketDomainTest.cs
+++ src/Tests/Modules/Baskets/Bazaar.Modules.Baskets.Tests.Unit/Bazaar.Modules.Baskets.Tests.Unit/Domain/BasketDomainTest.cs
@@ -1,6 +1,80 @@
-﻿namespace Bazaar.Modules.Baskets.Tests.Unit.Domain
+﻿using Modules.Baskets.Domain.Entities;
+using Shared.Domain.Exceptions;
+
+namespace Bazaar.Modules.Baskets.Tests.Unit.Domain
 {
-    internal class BasketDomainTest
+    public class BasketDomainTest
     {
+        [Fact]
+        public void CreateBasket_ReturnsBasket_IfParamsValid()
+        {
+            var basket = Basket.CreateBasket(Guid.NewGuid(), "USD");
+
+            Assert.NotNull(basket);
+            Assert.IsType<Basket>(basket);
+        }
+
+        [Fact]
+        public void AddProductToBasket_ReturnsIfSuccessfully()
+        {
+            var product = BasketFactory.GetProduct();
+            var basket = BasketFactory.GetBasket();
+
+            basket.AddProductToBasket(product.Id, product.ShopId, 1, product.Price, product.Price.Amount);
+
+
+            var basketItem = basket.Items.FirstOrDefault(x => x.ProductId.Value == product.Id.Value);
+
+            Assert.Equal(1, basketItem?.Quantity);
+            Assert.Equal(product.Id.Value, basketItem?.ProductId.Value);
+            Assert.IsType<BasketItem>(basketItem);
+        }
+
+        [Fact]
+        public void RemoveProductFromBasket_RemovesBasketItemFromBasket()
+        {
+            var product = BasketFactory.GetProduct();
+            var basket = BasketFactory.GetBasket();
+
+            basket.AddProductToBasket(product.Id, product.ShopId, 1, product.Price, product.Price.Amount);
+
+            var basketItem = basket.Items.FirstOrDefault(x => x.ProductId.Value == product.Id.Value);
+
+            Assert.Equal(product.Id.Value, basketItem?.ProductId.Value);
+            Assert.IsType<BasketItem>(basketItem);
+
+            basket.RemoveItemFromCart(basketItem.Id);
+            var item = basket.Items.FirstOrDefault(x => x.Id == basketItem.Id);
+
+            Assert.IsNotType<BasketItem>(item);
+            Assert.Null(item);
+        }
+
+        [Fact]
+        public void ChangeBasketCurrency_ChangesCurrency_IfCurrencyAvailable()
+        {
+            decimal conversionRate = 4.0M;
+            var product = BasketFactory.GetProduct();
+            var basket = BasketFactory.GetBasket();
+
+            basket.AddProductToBasket(product.Id, product.ShopId, 1, product.Price, product.Price.Amount);
+
+            basket.ChangeBasketCurrency(conversionRate, "PLN");
+
+            Assert.Equal("PLN", basket.TotalPrice.Currency);
+        }
+
+        [Fact]
+        public void ChangeBasketCurrency_Throws_IfCurrencyNotAcceptedByTheSystem()
+        {
+            decimal conversionRate = 4.0M;
+            var product = BasketFactory.GetProduct();
+            var basket = BasketFactory.GetBasket();
+
+            basket.AddProductToBasket(product.Id, product.ShopId, 1, product.Price, product.Price.Amount);
+
+            var act = Assert.Throws<InvalidPriceException>(() => basket.ChangeBasketCurrency(conversionRate, "PES"));
+            Assert.IsType<InvalidPriceException>(act);
+        }
     }
 }
--- src/Tests/Modules/Baskets/Bazaar.Modules.Baskets.Tests.Unit/Bazaar.Modules.Baskets.Tests.Unit/Domain/BasketFactory.cs
+++ src/Tests/Modules/Baskets/Bazaar.Modules.Baskets.Tests.Unit/Bazaar.Modules.Baskets.Tests.Unit/Domain/BasketFactory.cs
@@ -1,6 +1,40 @@
-﻿namespace Bazaar.Modules.Baskets.Tests.Unit.Domain
+﻿using Modules.Baskets.Domain.Entities;
+using Modules.Products.Domain.Entities;
+using Modules.Products.Domain.ValueObjects;
+using Shared.Domain.ValueObjects;
+
+namespace Bazaar.Modules.Baskets.Tests.Unit.Domain
 {
-    internal class BasketFactory
+    public static class BasketFactory
     {
+        public static Basket GetBasket()
+        {
+            var basket = Basket.CreateBasket(Guid.NewGuid(), "USD");
+
+            return basket;
+        }
+
+        public static Basket GetBasketWithItems()
+        {
+            var basket = Basket.CreateBasket(Guid.NewGuid(), "USD");
+            var product = GetProduct();
+
+            basket.AddProductToBasket(product.Id, product.ShopId, 1, product.Price, product.Price.Amount);
+
+            return basket;
+        }
+
+        public static Product GetProduct()
+        {
+            var product = Product.CreateProduct("Name",
+                                                "Description",
+                                                ProductCategory.Create("Food"),
+                                                1,
+                                                MoneyValue.Of(10, "USD"),
+                                                "Piece",
+                                                Guid.NewGuid());
+
+            return product;
+        }
     }
 }
