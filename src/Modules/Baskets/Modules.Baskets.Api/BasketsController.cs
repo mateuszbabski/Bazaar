235a72593f28c71c61ea1b1acf8d33c05d1d0d7a
mateuszbabski <babskimateusz@gmail.com>
2023-07-19 23:16:32 +0200

    Baskets controller Delete + Change currency

--- src/Modules/Baskets/Modules.Baskets.Api/BasketsController.cs
+++ src/Modules/Baskets/Modules.Baskets.Api/BasketsController.cs
@@ -1,5 +1,11 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Modules.Baskets.Application.Commands.ChangeBasketCurrency;
+using Modules.Baskets.Application.Commands.DeleteBasket;
+using Shared.Domain.ValueObjects;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace Modules.Baskets.Api
 {
@@ -12,6 +18,34 @@ namespace Modules.Baskets.Api
         public BasketsController(IMediator mediator)
         {
             _mediator = mediator;
+        }
+
+        [Authorize(Roles = "customer")]
+        [HttpPost("ChangeBasketCurrency")]
+        [SwaggerOperation("Change basket currency")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Guid>> ChangeBasketCurrency(ChangeBasketCurrencyCommand command)
+        {
+            var result = await _mediator.Send(command);
+
+            return Ok(result);
+        }
+
+        [Authorize(Roles = "customer")]
+        [HttpPost("DeleteBasket")]
+        [SwaggerOperation("Delete basket")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Guid>> DeleteBasket(DeleteBasketCommand command)
+        {
+            await _mediator.Send(command);
+
+            return NoContent();
         }
     }
 }
