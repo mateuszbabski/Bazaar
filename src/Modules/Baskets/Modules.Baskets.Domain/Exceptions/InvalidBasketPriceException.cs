70cf143822be2c8388d41a6ee47e1d8f3f9e3b74
mateuszbabski <babskimateusz@gmail.com>
2023-07-19 22:35:17 +0200

    Invalid basket price exception

---
+++ src/Modules/Baskets/Modules.Baskets.Domain/Exceptions/InvalidBasketPriceException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modules.Baskets.Domain.Exceptions
+{
+    public class InvalidBasketPriceException : Exception
+    {
+        public InvalidBasketPriceException(string message) : base(message)
+        {
+
+        }
+    }
+}
