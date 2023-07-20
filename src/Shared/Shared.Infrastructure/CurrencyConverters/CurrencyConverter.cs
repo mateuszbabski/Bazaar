8b63a10a590d355e83291b9bd433534d48c5dd3a
mateuszbabski <babskimateusz@gmail.com>
2023-07-20 22:05:45 +0200

    IHttpClientFactor instead of HttpClient

--- src/Shared/Shared.Infrastructure/CurrencyConverters/CurrencyConverter.cs
+++ src/Shared/Shared.Infrastructure/CurrencyConverters/CurrencyConverter.cs
@@ -1,25 +1,28 @@
 ﻿using Shared.Abstractions.CurrencyConverters;
+using System.Net.Http;
 using System.Net.Http.Json;
 
 namespace Shared.Infrastructure.CurrencyConverters
 {
     internal sealed class CurrencyConverter : ICurrencyConverter
     {
-        private readonly HttpClient _httpClient;
+        private readonly IHttpClientFactory _httpClientFactory;
 
-        public CurrencyConverter(HttpClient httpClient)
+        public CurrencyConverter(IHttpClientFactory httpClientFactory)
         {
-            _httpClient = httpClient;
+            _httpClientFactory = httpClientFactory;
         }
 
         public async Task<decimal> GetConversionRate(string from, string to)
         {
             if (from == to) return 1;
 
+            var httpClient = _httpClientFactory.CreateClient();
+
             var uriString =
                 string.Format($"https://api.frankfurter.app/latest?amount=1&from={@from}&to={@to}", from, to);
 
-            var response = await _httpClient.GetAsync(uriString);
+            var response = await httpClient.GetAsync(uriString);
             response.EnsureSuccessStatusCode();
 
             var serializedResponse = await response.Content.ReadFromJsonAsync<ConversionResult>();
@@ -31,10 +34,12 @@ namespace Shared.Infrastructure.CurrencyConverters
         {
             if (from == to) return amount;
 
+            var httpClient = _httpClientFactory.CreateClient();
+
             var uriString =
                 string.Format($"https://api.frankfurter.app/latest?amount={@amount}&from={@from}&to={@to}", amount, from, to);
 
-            var response = await _httpClient.GetAsync(uriString);
+            var response = await httpClient.GetAsync(uriString);
             response.EnsureSuccessStatusCode();
 
             var serializedResponse = await response.Content.ReadFromJsonAsync<ConversionResult>();
