78119d820f8bbba37a3fb7684db0e3ed0aff5ef0
mateuszbabski <babskimateusz@gmail.com>
2023-07-24 17:19:41 +0200

    Deleted shared app events

--- src/Shared/Shared.Application/IntegrationEvents/BasketCheckedOut/BasketCheckedOutEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿using Shared.Abstractions.Events;
-
-namespace Shared.Application.IntegrationEvents.BasketCheckedOut
-{
-    public record BasketCheckedOutEvent(BasketCheckoutMessage Message) : IEvent
-    {
-    }
-
-}
