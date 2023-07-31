5ba2a23bebaaa69f365f130b829c4e5522c0a5f3
mateuszbabski <babskimateusz@gmail.com>
2023-07-31 08:42:54 +0200

    Shipping domain layer entities init

---
+++ src/Modules/Shippings/Modules.Shippings.Domain/Entities/Shipping.cs
@@ -0,0 +1,58 @@
+﻿using Modules.Shippings.Domain.Events;
+using Modules.Shippings.Domain.ValueObjects;
+using Shared.Domain;
+using Shared.Domain.ValueObjects;
+
+namespace Modules.Shippings.Domain.Entities
+{
+    public class Shipping : Entity, IAggregateRoot
+    {
+        public ShippingId Id { get; private set; }
+        public Guid OrderId { get; private set; }
+        public ShippingMethodId ShippingMethodId { get; private set; }
+        public TrackingNumber TrackingNumber { get; private set; }
+        public ShippingStatus Status { get; private set; }
+        public DateTimeOffset CreatedDate { get; private set; } = DateTimeOffset.Now;
+        public DateTimeOffset LastUpdateDate { get; private set; } = DateTimeOffset.Now;
+        public Weight TotalWeight { get; private set; } 
+        public MoneyValue TotalPrice { get; private set; }
+
+        private Shipping()
+        {
+            
+        }
+
+        internal Shipping(Guid orderId,
+                          ShippingMethodId methodId,
+                          DateTimeOffset createdOn,
+                          decimal weight,
+                          MoneyValue baseShippingPrice)
+        {
+            Id = new ShippingId(Guid.NewGuid());
+            OrderId = orderId;
+            ShippingMethodId = methodId;
+            Status = ShippingStatus.Pending;
+            CreatedDate = createdOn;
+            TrackingNumber = new TrackingNumber(Guid.NewGuid());
+            TotalWeight = weight;
+            TotalPrice = CalculateTotalPrice(weight, baseShippingPrice);
+        }
+
+        public static Shipping CreateShipping(Guid orderId,
+                                              ShippingMethodId methodId,
+                                              DateTimeOffset createdOn,
+                                              decimal weight,
+                                              MoneyValue baseShippingPrice)
+        {
+            var shipping = new Shipping(orderId, methodId, createdOn, weight, baseShippingPrice);
+            shipping.AddDomainEvent(new ShippingCreatedDomainEvent(shipping.Id));
+            return shipping;
+        }
+
+        private MoneyValue CalculateTotalPrice(decimal weight, MoneyValue baseShippingPrice)
+        {            
+            return TotalPrice;
+        }
+    }
+
+}
---
+++ src/Modules/Shippings/Modules.Shippings.Domain/Entities/ShippingMethod.cs
@@ -0,0 +1,37 @@
+﻿using Modules.Shippings.Domain.Events;
+using Modules.Shippings.Domain.ValueObjects;
+using Shared.Domain;
+using Shared.Domain.ValueObjects;
+
+namespace Modules.Shippings.Domain.Entities
+{
+    public class ShippingMethod : Entity
+    {
+        public ShippingMethodId Id { get; private set; }
+        public ShippingMethodName Name { get; private set; }
+        public MoneyValue BasePrice { get; private set; }
+        public int DurationTimeInDays { get; private set; } = 1;
+
+        private ShippingMethod()
+        {
+            
+        }
+
+        internal ShippingMethod(ShippingMethodName name, MoneyValue basePrice, int durationTime)
+        {
+            Id = new ShippingMethodId(Guid.NewGuid());
+            Name = name;
+            BasePrice = basePrice;
+            DurationTimeInDays = durationTime;
+        }
+
+        public static ShippingMethod CreateNewShippingMethod(string name, MoneyValue basePrice, int durationTime)
+        {
+            var shippingMethod = new ShippingMethod(name, basePrice, durationTime);
+
+            shippingMethod.AddDomainEvent(new NewShippingMethodCreatedDomainEvent(shippingMethod.Id));
+
+            return shippingMethod;
+        }
+    }
+}
