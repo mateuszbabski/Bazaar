84e939690b9ad4558aa6dbd4ea52a2d3e2852d98
mateuszbabski <babskimateusz@gmail.com>
2023-07-31 08:43:06 +0200

    Shippings layer domain events init

---
+++ src/Modules/Shippings/Modules.Shippings.Domain/Events/NewShippingMethodCreatedDomainEvent.cs
@@ -0,0 +1,9 @@
+﻿using Modules.Shippings.Domain.ValueObjects;
+using Shared.Domain;
+
+namespace Modules.Shippings.Domain.Events
+{
+    public record NewShippingMethodCreatedDomainEvent(ShippingMethodId Id) : IDomainEvent
+    {
+    }
+}
---
+++ src/Modules/Shippings/Modules.Shippings.Domain/Events/ShippingCreatedDomainEvent.cs
@@ -0,0 +1,8 @@
+﻿using Shared.Domain;
+
+namespace Modules.Shippings.Domain.Events
+{
+    public record ShippingCreatedDomainEvent(Guid Id) : IDomainEvent
+    {
+    }
+}
