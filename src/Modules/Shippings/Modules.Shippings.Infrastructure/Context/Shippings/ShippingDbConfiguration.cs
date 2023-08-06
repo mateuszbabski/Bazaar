b9e06b46d9c7fae7b7723c9ef87c9a60ab5a9816
mateuszbabski <babskimateusz@gmail.com>
2023-08-06 16:38:19 +0200

    Shippings context updated

--- src/Modules/Shippings/Modules.Shippings.Infrastructure/Context/Shippings/ShippingDbConfiguration.cs
+++ src/Modules/Shippings/Modules.Shippings.Infrastructure/Context/Shippings/ShippingDbConfiguration.cs
@@ -1,15 +1,39 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Modules.Shippings.Domain.Entities;
+using Modules.Shippings.Domain.ValueObjects;
 
 namespace Modules.Shippings.Infrastructure.Context.Shippings
 {
-    internal class ShippingDbConfiguration : IEntityTypeConfiguration<Shipping>
+    internal sealed class ShippingDbConfiguration : IEntityTypeConfiguration<Shipping>
     {
-        // TODO: ShippingDbConfiguration 
         public void Configure(EntityTypeBuilder<Shipping> builder)
         {
-            throw new NotImplementedException();
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Id)
+                   .HasConversion(c => c.Value, c => new ShippingId(c));
+
+            builder.OwnsOne(c => c.TotalPrice, mv =>
+            {
+                mv.Property(p => p.Currency).HasMaxLength(3).HasColumnName("Currency");
+                mv.Property(p => p.Amount).HasColumnName("Amount").HasPrecision(18, 2);
+            });
+
+            builder.Property(c => c.OrderId)
+                   .HasConversion(c => c.Value, c => new ShippingOrderId(c));
+
+            builder.Property(c => c.ShippingMethodId)
+                   .HasConversion(c => c.Value, c => new ShippingMethodId(c));
+
+            builder.Property(c => c.TrackingNumber)
+                   .HasConversion(c => c.Value, c => new TrackingNumber(c));
+
+            builder.Property(c => c.Status).HasColumnName("ShippingStatus");
+            builder.Property(c => c.TotalWeight).HasColumnName("TotalWeight");
+            builder.Property(c => c.CreatedDate).HasColumnName("CreatedDate");
+            builder.Property(c => c.LastUpdateDate).HasColumnName("LastUpdatedDate");
+
+            builder.ToTable("Shippings");
         }
     }
 }
