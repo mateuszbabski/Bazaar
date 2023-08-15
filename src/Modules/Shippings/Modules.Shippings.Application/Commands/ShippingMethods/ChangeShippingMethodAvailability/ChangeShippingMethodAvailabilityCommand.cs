f951ada7c62028eecc94d5329393995f389edd54
mateuszbabski <babskimateusz@gmail.com>
2023-08-15 14:39:06 +0200

    Change shipping method availability

---
+++ src/Modules/Shippings/Modules.Shippings.Application/Commands/ShippingMethods/ChangeShippingMethodAvailability/ChangeShippingMethodAvailabilityCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Modules.Shippings.Application.Commands.ShippingMethods.ChangeShippingMethodAvailability
+{
+    public class ChangeShippingMethodAvailabilityCommand : IRequest<Unit>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ src/Modules/Shippings/Modules.Shippings.Application/Commands/ShippingMethods/ChangeShippingMethodAvailability/ChangeShippingMethodAvailabilityCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Modules.Shippings.Domain.Repositories;
+using Shared.Abstractions.UnitOfWork;
+using Shared.Application.Exceptions;
+
+namespace Modules.Shippings.Application.Commands.ShippingMethods.ChangeShippingMethodAvailability
+{
+    internal class ChangeShippingMethodAvailabilityCommandHandler : IRequestHandler<ChangeShippingMethodAvailabilityCommand, Unit>
+    {
+        private readonly IShippingMethodRepository _shippingMethodRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ChangeShippingMethodAvailabilityCommandHandler(IShippingMethodRepository shippingMethodRepository,
+                                                              IUnitOfWork unitOfWork)
+        {
+            _shippingMethodRepository = shippingMethodRepository;
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<Unit> Handle(ChangeShippingMethodAvailabilityCommand command, CancellationToken cancellationToken)
+        {
+            var shippingMethod = await _shippingMethodRepository.GetShippingMethodById(command.Id)
+                ?? throw new NotFoundException("Shipping method not found.");
+
+            shippingMethod.ChangeAvailability();
+
+            await _unitOfWork.CommitAndDispatchDomainEventsAsync(shippingMethod);
+
+            return Unit.Value;
+        }
+    }
+}
