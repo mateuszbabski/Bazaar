5413c32186e709d91baa9e658d708a8b4770ef3e
mateuszbabski <babskimateusz@gmail.com>
2023-08-15 14:39:37 +0200

    Get shipping methods

---
+++ src/Modules/Shippings/Modules.Shippings.Application/Queries/ShippingMethods/GetShippingMethods/GetAllShippingMethodsQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Modules.Shippings.Application.Dtos;
+using Shared.Application.Queries;
+
+namespace Modules.Shippings.Application.Queries.ShippingMethods.GetShippingMethods
+{
+    public class GetAllShippingMethodsQuery : IRequest<PagedList<ShippingMethodDto>>
+    {
+        #nullable enable
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+        public string? SortBy { get; set; }
+        public string SortDirection { get; set; } = "ASC";
+    }
+}
---
+++ src/Modules/Shippings/Modules.Shippings.Application/Queries/ShippingMethods/GetShippingMethods/GetAllShippingMethodsQueryHandler.cs
@@ -0,0 +1,45 @@
+﻿using MediatR;
+using Modules.Shippings.Application.Dtos;
+using Modules.Shippings.Domain.Entities;
+using Modules.Shippings.Domain.Repositories;
+using Shared.Abstractions.Queries;
+using Shared.Application.Queries;
+
+namespace Modules.Shippings.Application.Queries.ShippingMethods.GetShippingMethods
+{
+    internal class GetAllShippingMethodsQueryHandler : IRequestHandler<GetAllShippingMethodsQuery, PagedList<ShippingMethodDto>>
+    {
+        private readonly IShippingMethodRepository _shippingMethodRepository;
+        private readonly IQueryProcessor<ShippingMethod> _queryProcessor;
+
+        public GetAllShippingMethodsQueryHandler(IShippingMethodRepository shippingMethodRepository,
+                                                 IQueryProcessor<ShippingMethod> queryProcessor)
+        {
+            _shippingMethodRepository = shippingMethodRepository;
+            _queryProcessor = queryProcessor;
+        }
+        public async Task<PagedList<ShippingMethodDto>> Handle(GetAllShippingMethodsQuery query, CancellationToken cancellationToken)
+        {
+            var baseQuery = await _shippingMethodRepository.GetShippingMethods();
+
+            var sortedQuery = _queryProcessor.SortQuery(baseQuery.AsQueryable(), query.SortBy, query.SortDirection);
+
+            var pagedShippingMethods = _queryProcessor.PageQuery(sortedQuery.AsEnumerable(), query.PageNumber, query.PageSize);
+
+            var shippingMethodListDto = new List<ShippingMethodDto>();
+
+            foreach(var shippingMethod in pagedShippingMethods)
+            {
+                var shippingMethodDto = ShippingMethodDto.CreateDtoFromObject(shippingMethod);
+                shippingMethodListDto.Add(shippingMethodDto);
+            }
+
+            var pagedShippingMethodList = new PagedList<ShippingMethodDto>(shippingMethodListDto,
+                                                                           baseQuery.Count(),
+                                                                           query.PageNumber,
+                                                                           query.PageSize);
+
+            return pagedShippingMethodList;
+        }
+    }
+}
