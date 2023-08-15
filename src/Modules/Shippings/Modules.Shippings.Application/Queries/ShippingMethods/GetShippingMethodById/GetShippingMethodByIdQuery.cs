f753ba9a6fca9169378f56f4b6144e366ede9849
mateuszbabski <babskimateusz@gmail.com>
2023-08-15 14:39:29 +0200

    Get shipping method by id

---
+++ src/Modules/Shippings/Modules.Shippings.Application/Queries/ShippingMethods/GetShippingMethodById/GetShippingMethodByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Modules.Shippings.Application.Dtos;
+
+namespace Modules.Shippings.Application.Queries.ShippingMethods.GetShippingMethodById
+{
+    public class GetShippingMethodByIdQuery : IRequest<ShippingMethodDto>
+    {
+        public Guid Id { get; set; }    
+    }
+}
---
+++ src/Modules/Shippings/Modules.Shippings.Application/Queries/ShippingMethods/GetShippingMethodById/GetShippingMethodByIdQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Modules.Shippings.Application.Dtos;
+using Modules.Shippings.Domain.Repositories;
+using Shared.Application.Exceptions;
+
+namespace Modules.Shippings.Application.Queries.ShippingMethods.GetShippingMethodById
+{
+    internal class GetShippingMethodByIdQueryHandler : IRequestHandler<GetShippingMethodByIdQuery, ShippingMethodDto>
+    {
+        private readonly IShippingMethodRepository _shippingMethodRepository;
+
+        public GetShippingMethodByIdQueryHandler(IShippingMethodRepository shippingMethodRepository)
+        {
+            _shippingMethodRepository = shippingMethodRepository;
+        }
+        public async Task<ShippingMethodDto> Handle(GetShippingMethodByIdQuery command, CancellationToken cancellationToken)
+        {
+            var shippingMethod = await _shippingMethodRepository.GetShippingMethodById(command.Id)
+                ?? throw new NotFoundException("Shipping method not found");
+
+            var shippingMethodDto = ShippingMethodDto.CreateDtoFromObject(shippingMethod);
+
+            return shippingMethodDto;
+        }
+    }
+}
