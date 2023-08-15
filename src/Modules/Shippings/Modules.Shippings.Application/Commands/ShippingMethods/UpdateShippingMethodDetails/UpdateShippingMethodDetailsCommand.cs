9c00c3c5985a57eb1154d0865dc6b55f27f2ab69
mateuszbabski <babskimateusz@gmail.com>
2023-08-15 14:39:19 +0200

    Update shipping method details

---
+++ src/Modules/Shippings/Modules.Shippings.Application/Commands/ShippingMethods/UpdateShippingMethodDetails/UpdateShippingMethodDetailsCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Modules.Shippings.Application.Commands.ShippingMethods.UpdateShippingMethodDetails
+{
+    public class UpdateShippingMethodDetailsCommand : IRequest<Unit>
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }    
+        public decimal Amount { get; set; }
+        public string Currency { get; set; }
+        public int DurationTime { get; set; }
+    }
+}
---
+++ src/Modules/Shippings/Modules.Shippings.Application/Commands/ShippingMethods/UpdateShippingMethodDetails/UpdateShippingMethodDetailsCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Modules.Shippings.Domain.Repositories;
+using Shared.Abstractions.UnitOfWork;
+using Shared.Application.Exceptions;
+
+namespace Modules.Shippings.Application.Commands.ShippingMethods.UpdateShippingMethodDetails
+{
+    internal class UpdateShippingMethodDetailsCommandHandler : IRequestHandler<UpdateShippingMethodDetailsCommand, Unit>
+    {
+        private readonly IShippingMethodRepository _shippingMethodRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UpdateShippingMethodDetailsCommandHandler(IShippingMethodRepository shippingMethodRepository,
+                                                         IUnitOfWork unitOfWork)
+        {
+            _shippingMethodRepository = shippingMethodRepository;
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<Unit> Handle(UpdateShippingMethodDetailsCommand command, CancellationToken cancellationToken)
+        {
+            var shippingMethod = await _shippingMethodRepository.GetShippingMethodById(command.Id)
+                ?? throw new NotFoundException("Shipping method not found.");
+
+            shippingMethod.UpdateDetails(command.Name,
+                                         command.Amount,
+                                         command.Currency,
+                                         command.DurationTime);
+
+            await _unitOfWork.CommitAndDispatchDomainEventsAsync(shippingMethod);
+
+            return Unit.Value;
+        }
+    }
+}
