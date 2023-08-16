1a527009dc34dd6a0a62a64f89644868891c339a
mateuszbabski <babskimateusz@gmail.com>
2023-08-16 21:32:20 +0200

    ShippingMethod Query Processor

---
+++ src/Modules/Shippings/Modules.Shippings.Application/Services/ShippingMethodQueryProcessor.cs
@@ -0,0 +1,35 @@
+﻿using Modules.Shippings.Domain.Entities;
+using Shared.Application.Queries;
+using System.Linq.Expressions;
+
+namespace Modules.Shippings.Application.Services
+{
+    internal class ShippingMethodQueryProcessor : QueryProcessor<ShippingMethod>
+    {
+        public override IQueryable<ShippingMethod> SortQuery(IQueryable<ShippingMethod> baseQuery, string sortBy, string sortDirection)
+        {
+            if (!string.IsNullOrEmpty(sortBy))
+            {
+                var columnsSelector = new Dictionary<string, Expression<Func<ShippingMethod, object>>>(StringComparer.OrdinalIgnoreCase)
+                {
+                    { nameof(ShippingMethod.Name), r => r.Name.Value },
+                    { nameof(ShippingMethod.BasePrice.Amount), r => r.BasePrice.Amount },
+                    { nameof(ShippingMethod.DurationTimeInDays), r => r.DurationTimeInDays }
+                };
+
+                if (columnsSelector.TryGetValue(sortBy, out var selectedColumn))
+                {
+                    baseQuery = sortDirection == "ASC"
+                            ? baseQuery.OrderBy(selectedColumn)
+                            : baseQuery.OrderByDescending(selectedColumn);
+                }
+                else
+                {
+                    return baseQuery;
+                }
+            }
+
+            return baseQuery;
+        }
+    }
+}
