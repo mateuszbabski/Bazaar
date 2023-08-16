b3efcaa9bb4adef523ab05a32d7d2b488d88d9d2
mateuszbabski <babskimateusz@gmail.com>
2023-08-16 21:30:41 +0200

    Shipping method entity - added methods

--- src/Modules/Shippings/Modules.Shippings.Domain/Entities/ShippingMethod.cs
+++ src/Modules/Shippings/Modules.Shippings.Domain/Entities/ShippingMethod.cs
@@ -1,4 +1,5 @@
 ﻿using Modules.Shippings.Domain.Events;
+using Modules.Shippings.Domain.Exceptions;
 using Modules.Shippings.Domain.ValueObjects;
 using Shared.Domain;
 using Shared.Domain.ValueObjects;
@@ -29,11 +30,76 @@ namespace Modules.Shippings.Domain.Entities
 
         public static ShippingMethod CreateNewShippingMethod(string name, MoneyValue basePrice, int durationTime)
         {
+            if (durationTime <= 0)
+            {
+                throw new InvalidDurationTimeException();
+            }
+
             var shippingMethod = new ShippingMethod(name, basePrice, durationTime);
 
             shippingMethod.AddDomainEvent(new NewShippingMethodCreatedDomainEvent(shippingMethod.Id));
 
             return shippingMethod;
+        }
+
+        public void UpdateDetails(string newName,
+                                  decimal amount,
+                                  string currency,
+                                  int newDurationTime)
+        {
+            SetName(newName);
+            SetPrice(amount, currency);
+            SetDurationTime(newDurationTime);
+        }
+
+        private void SetName(string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+                this.Name = new ShippingMethodName(name);
+        }
+
+        private void SetPrice(decimal amount, string currency)
+        {
+            if (amount <= 0)
+            {
+                amount = this.BasePrice.Amount;
+            }
+
+            if (string.IsNullOrEmpty(currency))
+            {
+                currency = this.BasePrice.Currency;
+            }
+
+            var newPrice = MoneyValue.Of(amount, currency);
+            this.BasePrice = newPrice;
+        }
+
+        private void SetDurationTime(int durationTime)
+        {
+            if (durationTime > 0)
+                this.DurationTimeInDays = durationTime;
+        }
+
+        public void ChangeAvailability()
+        {
+            if (IsAvailable == true)
+            {
+                Remove();
+            }
+            else
+            {
+                Restore();
+            }
+        }
+
+        private void Remove()
+        {
+            this.IsAvailable = false;
+        }
+
+        private void Restore()
+        {
+            this.IsAvailable = true;
         }
     }
 }
