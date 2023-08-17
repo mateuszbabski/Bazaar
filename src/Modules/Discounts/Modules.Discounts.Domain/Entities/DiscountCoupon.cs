b522db93ab95109f2dadf021490ca13a126d2edb
mateuszbabski <babskimateusz@gmail.com>
2023-08-17 20:43:27 +0200

    changed createdAt to startsAt in discount coupon

--- src/Modules/Discounts/Modules.Discounts.Domain/Entities/DiscountCoupon.cs
+++ src/Modules/Discounts/Modules.Discounts.Domain/Entities/DiscountCoupon.cs
@@ -7,22 +7,22 @@ namespace Modules.Discounts.Domain.Entities
     {
         public DiscountCouponId Id { get; private set; }
         public DiscountCode DiscountCode { get; private set; }
-        public DateTimeOffset CreatedAt { get; private set; } = DateTimeOffset.Now;
+        public DateTimeOffset StartsAt { get; private set; } = DateTimeOffset.Now;
         public DateTimeOffset ExpirationDate { get; private set; }
         public bool IsEnable { get; private set; } = true;
 
-        private DiscountCoupon(DateTimeOffset createdOn, DateTimeOffset expirationDate) 
+        private DiscountCoupon(DateTimeOffset startsAt, DateTimeOffset expirationDate) 
         {
             Id = new DiscountCouponId(Guid.NewGuid());
             DiscountCode = new DiscountCode(Guid.NewGuid());
-            CreatedAt = createdOn;
+            StartsAt = startsAt;
             ExpirationDate = expirationDate;
             IsEnable = true;
         } 
 
-        public static DiscountCoupon CreateDiscountCoupon(DateTimeOffset createdOn, DateTimeOffset expirationDate)
+        public static DiscountCoupon CreateDiscountCoupon(DateTimeOffset startsAt, DateTimeOffset expirationDate)
         {
-            var coupon = new DiscountCoupon(createdOn, expirationDate);
+            var coupon = new DiscountCoupon(startsAt, expirationDate);
             return coupon;
         }
 
