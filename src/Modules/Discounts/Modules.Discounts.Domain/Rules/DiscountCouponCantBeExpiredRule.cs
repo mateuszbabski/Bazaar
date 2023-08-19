420346eec1b58e1c86f92c9ec80ddd71bf276c38
mateuszbabski <babskimateusz@gmail.com>
2023-08-19 07:33:19 +0200

    Discount domain layer buisness rules

---
+++ src/Modules/Discounts/Modules.Discounts.Domain/Rules/DiscountCouponCantBeExpiredRule.cs
@@ -0,0 +1,24 @@
+﻿using Shared.Domain;
+
+namespace Modules.Discounts.Domain.Rules
+{
+    public class DiscountCouponCantBeExpiredRule : IBusinessRule
+    {
+        private readonly DateTimeOffset _expires;
+        private readonly DateTimeOffset _utcNow;
+
+        public DiscountCouponCantBeExpiredRule(DateTimeOffset expires, DateTimeOffset utcNow)
+        {
+            _expires = expires;
+            _utcNow = utcNow;
+        }
+
+        public string Message => "Discount Coupon has expired";
+
+        public bool IsBroken()
+        {
+            return _expires <= _utcNow;
+        }
+        
+    }
+}
---
+++ src/Modules/Discounts/Modules.Discounts.Domain/Rules/DiscountCouponHasToStartBeforeExpirationDateRule.cs
@@ -0,0 +1,23 @@
+﻿using Shared.Domain;
+
+namespace Modules.Discounts.Domain.Rules
+{
+    public class DiscountCouponHasToStartBeforeExpirationDateRule : IBusinessRule
+    {
+        private readonly DateTimeOffset _expires;
+        private readonly DateTimeOffset _starts;
+
+        public DiscountCouponHasToStartBeforeExpirationDateRule(DateTimeOffset expires, DateTimeOffset starts)
+        {
+            _expires = expires;
+            _starts = starts;
+        }
+
+        public string Message => "Discount Coupon has expired";
+
+        public bool IsBroken()
+        {
+            return _starts >= _expires;
+        }
+    }
+}
