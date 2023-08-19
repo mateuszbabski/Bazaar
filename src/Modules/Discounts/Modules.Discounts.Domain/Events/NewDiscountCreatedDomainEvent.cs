1accfeee327ca208d324ccc9df52e214f8bb48ca
mateuszbabski <babskimateusz@gmail.com>
2023-08-19 07:32:54 +0200

    Discount domain layer domain events

---
+++ src/Modules/Discounts/Modules.Discounts.Domain/Events/NewDiscountCreatedDomainEvent.cs
@@ -0,0 +1,9 @@
+﻿using Modules.Discounts.Domain.ValueObjects;
+using Shared.Domain;
+
+namespace Modules.Discounts.Domain.Events
+{
+    public record NewDiscountCreatedDomainEvent(DiscountId DiscountId) : IDomainEvent
+    {
+    }
+}
