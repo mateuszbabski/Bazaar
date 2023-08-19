5e274ef5b131e0a3f3409e3dd2efed9ca862997d
mateuszbabski <babskimateusz@gmail.com>
2023-08-19 07:33:34 +0200

    Discount domain layer repositories

---
+++ src/Modules/Discounts/Modules.Discounts.Domain/Repositories/IDiscountCouponRepository.cs
@@ -0,0 +1,12 @@
+﻿using Modules.Discounts.Domain.Entities;
+using Modules.Discounts.Domain.ValueObjects;
+
+namespace Modules.Discounts.Domain.Repositories
+{
+    public interface IDiscountCouponRepository
+    {
+        Task<DiscountCoupon> Add(DiscountCoupon discountCoupon);
+        Task<DiscountCoupon> GetDiscountCouponById(DiscountCouponId id);
+        Task<IEnumerable<DiscountCoupon>> GetAll();
+    }
+}
---
+++ src/Modules/Discounts/Modules.Discounts.Domain/Repositories/IDiscountRepository.cs
@@ -0,0 +1,12 @@
+﻿using Modules.Discounts.Domain.Entities;
+using Modules.Discounts.Domain.ValueObjects;
+
+namespace Modules.Discounts.Domain.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Discount> Add(Discount discount);
+        Task<Discount> GetDiscountById(DiscountId id);
+        Task<IEnumerable<Discount>> GetAll();
+    }
+}
