087e38b9b1986d4f60d15a00584f02b3db1f46dd
mateuszbabski <babskimateusz@gmail.com>
2023-08-19 07:33:08 +0200

    Discount domain layer exceptions

---
+++ src/Modules/Discounts/Modules.Discounts.Domain/Exceptions/InvalidDiscountCouponExpirationDateException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modules.Discounts.Domain.Exceptions
+{
+    public class InvalidDiscountCouponExpirationDateException : Exception
+    {
+        public InvalidDiscountCouponExpirationDateException() : base(message: "Coupon can't start after expiration date")
+        {
+            
+        }
+    }
+}
