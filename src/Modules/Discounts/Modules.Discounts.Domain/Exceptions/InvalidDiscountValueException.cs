90e070b7d0500cff6025376bb6e23c1a8e7f0d50
mateuszbabski <babskimateusz@gmail.com>
2023-08-20 10:33:50 +0200

    Discount domain layer added exceptions

---
+++ src/Modules/Discounts/Modules.Discounts.Domain/Exceptions/InvalidDiscountValueException.cs
@@ -0,0 +1,10 @@
+﻿namespace Modules.Discounts.Domain.Exceptions
+{
+    public class InvalidDiscountValueException : Exception
+    {
+        public InvalidDiscountValueException() : base(message: "Discount value cannot be zero or negative.")
+        {
+            
+        }
+    }
+}
---
+++ src/Modules/Discounts/Modules.Discounts.Domain/Exceptions/MissingRequiredTargetIdException.cs
@@ -0,0 +1,10 @@
+﻿namespace Modules.Discounts.Domain.Exceptions
+{
+    public class MissingRequiredTargetIdException : Exception
+    {
+        public MissingRequiredTargetIdException() : base(message: "Chosen discount type needs target id")
+        {
+            
+        }
+    }
+}
