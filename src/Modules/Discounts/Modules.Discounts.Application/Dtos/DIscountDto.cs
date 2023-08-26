77e6eee3fd9d49c6b328e981120b328d9e0ca6d8
mateuszbabski <babskimateusz@gmail.com>
2023-08-26 21:41:29 +0200

    Discount dto + extensions

--- src/Modules/Discounts/Modules.Discounts.Application/Dtos/DIscountDto.cs
+++ src/Modules/Discounts/Modules.Discounts.Application/Dtos/DIscountDto.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Modules.Discounts.Domain.Entities;
 
 namespace Modules.Discounts.Application.Dtos
 {
-    internal class DIscountDto
+    public record DiscountDto
     {
+        public Guid Id { get; init; }
+        public Guid CreatedBy { get; init; }
+        public decimal DiscountValue { get; init; }
+        public bool IsPercentageDiscount { get; init; }
+        public string? Currency { get; init; }
+        public string DiscountType { get; init; }
+        public Guid? DiscountTarget { get; init; }
+
+        internal static IEnumerable<DiscountDto> CreateDtoFromObjects(List<Discount> discounts)
+        {
+            var discountList = new List<DiscountDto>();
+
+            foreach (var discount in discounts)
+            {
+                var discountDto = new DiscountDto()
+                {
+                    Id = discount.Id,
+                    CreatedBy = discount.CreatedBy,
+                    DiscountValue = discount.DiscountValue, 
+                    IsPercentageDiscount = discount.IsPercentageDiscount,
+                    Currency = discount.Currency,
+                    DiscountType = discount.DiscountTarget.DiscountType.ToString(),
+                    DiscountTarget = discount.DiscountTarget.TargetId
+                };
+
+                discountList.Add(discountDto);
+            }
+
+            return discountList;
+        }
     }
 }
--- src/Modules/Discounts/Modules.Discounts.Application/Extensions.cs
+++ src/Modules/Discounts/Modules.Discounts.Application/Extensions.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Modules.Discounts.Application.Contracts;
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using Modules.Discounts.Application.Commands.Discounts.CreateDiscount;
+using Shared.Application.Queries;
+using Modules.Discounts.Application.Services;
+using Modules.Discounts.Domain.Entities;
 
 namespace Modules.Discounts.Application
 {
@@ -7,6 +11,11 @@ namespace Modules.Discounts.Application
     {
         public static IServiceCollection AddDiscountsApplication(this IServiceCollection services)
         {
+            services.AddScoped<IValidator<CreateDiscountCommand>, CreateDiscountValidator>();
+            //services.AddScoped<IValidator<CreateDiscountCouponCommand>, CreateDiscountCouponValidator>();
+
+            services.AddQueryProcessor<DiscountQueryProcessor, Discount>();
+            services.AddQueryProcessor<DiscountCouponQueryProcessor, DiscountCoupon>();
 
             return services;
         }
