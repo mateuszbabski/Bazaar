670847850c42a71ce4cf92fae237ebea4bc62b08
mateuszbabski <babskimateusz@gmail.com>
2023-08-26 20:18:00 +0200

    Create Discount Command

---
+++ src/Modules/Discounts/Modules.Discounts.Application/Commands/Discounts/CreateDiscount/CreateDiscountCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Modules.Discounts.Domain.ValueObjects;
+
+namespace Modules.Discounts.Application.Commands.Discounts.CreateDiscount
+{
+    public class CreateDiscountCommand : IRequest<Guid>
+    {
+        #nullable enable
+        public decimal DiscountValue { get; set; }
+        public bool IsPercentageDiscount { get; set; } = true;
+        public string? Currency { get; set; } = string.Empty;
+        public DiscountType DiscountType { get; set; }
+        public Guid? DiscountTargetId { get; set; } = Guid.Empty;
+
+    }
+}
---
+++ src/Modules/Discounts/Modules.Discounts.Application/Commands/Discounts/CreateDiscount/CreateDiscountValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace Modules.Discounts.Application.Commands.Discounts.CreateDiscount
+{
+    public class CreateDiscountValidator : AbstractValidator<CreateDiscountCommand>
+    {
+        public CreateDiscountValidator()
+        {
+            RuleFor(c => c.DiscountValue).NotEmpty()
+                                         .WithMessage("Product name cannot be empty");
+
+            RuleFor(c => c.IsPercentageDiscount).NotEmpty()
+                                                .WithMessage("Description cannot be empty");
+
+            RuleFor(c => c.DiscountType).NotEmpty()
+                                        .WithMessage("Category cannot be empty");
+        }
+    }
+}
