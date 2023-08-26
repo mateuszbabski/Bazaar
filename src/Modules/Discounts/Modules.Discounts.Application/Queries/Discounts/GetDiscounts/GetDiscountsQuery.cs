6af7f3fe1020e6a4fe6e42336cc5f7f54d14fc71
mateuszbabski <babskimateusz@gmail.com>
2023-08-26 21:40:36 +0200

    Get Discounts Query Handler

--- src/Modules/Discounts/Modules.Discounts.Application/Queries/Discounts/GetDiscounts/GetDiscountsQuery.cs
+++ src/Modules/Discounts/Modules.Discounts.Application/Queries/Discounts/GetDiscounts/GetDiscountsQuery.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
+using Modules.Discounts.Application.Dtos;
+using Shared.Application.Queries;
 
 namespace Modules.Discounts.Application.Queries.Discounts.GetDiscounts
 {
-    internal class GetDiscountsQuery
+    public class GetDiscountsQuery : IRequest<PagedList<DiscountDto>>
     {
+        #nullable enable
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+        public string? SortBy { get; set; }
+        public string SortDirection { get; set; } = "ASC";
     }
 }
--- src/Modules/Discounts/Modules.Discounts.Application/Queries/Discounts/GetDiscounts/GetDiscountsQueryHandler.cs
+++ src/Modules/Discounts/Modules.Discounts.Application/Queries/Discounts/GetDiscounts/GetDiscountsQueryHandler.cs
@@ -1,12 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
+using Modules.Discounts.Application.Dtos;
+using Modules.Discounts.Domain.Entities;
+using Modules.Discounts.Domain.Repositories;
+using Shared.Abstractions.Queries;
+using Shared.Abstractions.UserServices;
+using Shared.Application.Exceptions;
+using Shared.Application.Queries;
 
 namespace Modules.Discounts.Application.Queries.Discounts.GetDiscounts
 {
-    internal class GetDiscountsQueryHandler
+    public class GetDiscountsQueryHandler : IRequestHandler<GetDiscountsQuery, PagedList<DiscountDto>>
     {
+        private readonly ICurrentUserService _currentUserService;
+        private readonly IDiscountRepository _discountRepository;
+        private readonly IQueryProcessor<Discount> _queryProcessor;
+
+        public GetDiscountsQueryHandler(ICurrentUserService currentUserService,
+                                        IDiscountRepository discountRepository,
+                                        IQueryProcessor<Discount> queryProcessor)
+        {
+            _currentUserService = currentUserService;
+            _discountRepository = discountRepository;
+            _queryProcessor = queryProcessor;
+        }
+        public async Task<PagedList<DiscountDto>> Handle(GetDiscountsQuery query, CancellationToken cancellationToken)
+        {
+            var userId = _currentUserService.UserId;
+
+            var baseQuery = await _discountRepository.GetAllCreatorDiscounts(userId)
+                ?? throw new NotFoundException("Discounts not found.");
+
+            var sortedQuery = _queryProcessor.SortQuery(baseQuery.AsQueryable(), query.SortBy, query.SortDirection);
+            var pagedDiscounts = _queryProcessor.PageQuery(sortedQuery.AsEnumerable(), query.PageNumber, query.PageSize);
+
+            var discountListDto = DiscountDto.CreateDtoFromObjects(pagedDiscounts);
+
+            var pagedDiscountList = new PagedList<DiscountDto>(discountListDto,
+                                                               baseQuery.Count(),
+                                                               query.PageNumber,
+                                                               query.PageSize);
+
+            return pagedDiscountList;
+        }
     }
 }
