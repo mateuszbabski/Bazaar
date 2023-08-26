980d78bbc566e15b7cc9409d4a637dd76ca2527a
mateuszbabski <babskimateusz@gmail.com>
2023-08-26 21:40:21 +0200

    Discount module query processors

--- src/Modules/Discounts/Modules.Discounts.Application/Services/DiscountQueryProcessor.cs
+++ src/Modules/Discounts/Modules.Discounts.Application/Services/DiscountQueryProcessor.cs
@@ -1,6 +1,35 @@
-﻿namespace Modules.Discounts.Application.Services
+﻿using Modules.Discounts.Domain.Entities;
+using Shared.Application.Queries;
+using System.Linq.Expressions;
+
+namespace Modules.Discounts.Application.Services
 {
-    internal class DiscountQueryProcessor
+    internal class DiscountQueryProcessor : QueryProcessor<Discount>
     {
+        public override IQueryable<Discount> SortQuery(IQueryable<Discount> baseQuery, string sortBy, string sortDirection)
+        {
+            if (!string.IsNullOrEmpty(sortBy))
+            {
+                var columnsSelector = new Dictionary<string, Expression<Func<Discount, object>>>(StringComparer.OrdinalIgnoreCase)
+                {
+                    { nameof(Discount.DiscountTarget.DiscountType), r => r.DiscountTarget.DiscountType },
+                    { nameof(Discount.IsPercentageDiscount), r => r.IsPercentageDiscount },
+                    { nameof(Discount.DiscountValue), r => r.DiscountValue }
+                };
+
+                if (columnsSelector.TryGetValue(sortBy, out var selectedColumn))
+                {
+                    baseQuery = sortDirection == "ASC"
+                            ? baseQuery.OrderBy(selectedColumn)
+                            : baseQuery.OrderByDescending(selectedColumn);
+                }
+                else
+                {
+                    return baseQuery;
+                }
+            }
+
+            return baseQuery;
+        }
     }
 }
