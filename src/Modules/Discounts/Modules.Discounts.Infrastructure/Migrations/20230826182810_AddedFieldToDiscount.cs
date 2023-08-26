2b52c458bdf5835572d29eb9e771dea7280b05a7
mateuszbabski <babskimateusz@gmail.com>
2023-08-26 21:40:09 +0200

    Added extension

---
+++ src/Modules/Discounts/Modules.Discounts.Infrastructure/Migrations/20230826182810_AddedFieldToDiscount.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Modules.Discounts.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedFieldToDiscount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Currency",
+                table: "Discounts",
+                type: "nvarchar(3)",
+                maxLength: 3,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CreatedBy",
+                table: "Discounts",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Discounts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Currency",
+                table: "Discounts",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(3)",
+                oldMaxLength: 3,
+                oldNullable: true);
+        }
+    }
+}
--- src/Modules/Products/Modules.Products.Infrastructure/Extensions.cs
+++ src/Modules/Products/Modules.Products.Infrastructure/Extensions.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Modules.Products.Application.Contracts;
+using Modules.Products.Contracts.Interfaces;
 using Modules.Products.Domain.Repositories;
 using Modules.Products.Infrastructure.Context;
 using Modules.Products.Infrastructure.Repository;
@@ -19,6 +20,7 @@ namespace Modules.Products.Infrastructure
 
             services.AddScoped<IProductsUnitOfWork, ProductsUnitOfWork>();
             services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductChecker, ProductRepository>();
 
             return services;
         }
