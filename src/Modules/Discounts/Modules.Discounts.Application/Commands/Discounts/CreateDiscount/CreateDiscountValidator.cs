9521f2c7dcc7c52597ac2e6d1153516c8c77c948
mateuszbabski <babskimateusz@gmail.com>
2023-08-29 21:07:49 +0200

    Small updates and corrections

--- src/Modules/Discounts/Modules.Discounts.Application/Commands/Discounts/CreateDiscount/CreateDiscountValidator.cs
+++ src/Modules/Discounts/Modules.Discounts.Application/Commands/Discounts/CreateDiscount/CreateDiscountValidator.cs
@@ -7,13 +7,10 @@ namespace Modules.Discounts.Application.Commands.Discounts.CreateDiscount
         public CreateDiscountValidator()
         {
             RuleFor(c => c.DiscountValue).NotEmpty()
-                                         .WithMessage("Product name cannot be empty");
-
-            RuleFor(c => c.IsPercentageDiscount).NotEmpty()
-                                                .WithMessage("Description cannot be empty");
+                                         .WithMessage("Discount Value cannot be empty");
 
             RuleFor(c => c.DiscountType).NotEmpty()
-                                        .WithMessage("Category cannot be empty");
+                                        .WithMessage("Discount Type cannot be empty");
         }
     }
 }
