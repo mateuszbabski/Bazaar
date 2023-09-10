c892b6fee4398fecc745e20e5f996fbb2624072e
mateuszbabski <babskimateusz@gmail.com>
2023-09-10 16:32:48 +0200

    Get DiscountCoupons query

--- src/Modules/Discounts/Modules.Discounts.Application/Queries/DiscountCoupons/GetDiscountCoupons/GetDiscountCouponsQuery.cs
+++ src/Modules/Discounts/Modules.Discounts.Application/Queries/DiscountCoupons/GetDiscountCoupons/GetDiscountCouponsQuery.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
+using Modules.Discounts.Application.Dtos;
+using Shared.Application.Queries;
 
 namespace Modules.Discounts.Application.Queries.DiscountCoupons.GetDiscountCoupons
 {
-    internal class GetDiscountCouponsQuery
+    public class GetDiscountCouponsQuery : IRequest<PagedList<DiscountCouponDto>>
     {
+        #nullable enable
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+        public string? SortBy { get; set; }
+        public string SortDirection { get; set; } = "ASC";
     }
 }
