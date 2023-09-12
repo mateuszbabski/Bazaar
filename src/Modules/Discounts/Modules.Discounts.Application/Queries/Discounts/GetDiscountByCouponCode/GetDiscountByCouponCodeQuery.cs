da28d78ac79b7148fde066e9a5435c8844580595
mateuszbabski <babskimateusz@gmail.com>
2023-09-12 20:41:05 +0200

    Added GetDiscountByCode query

---
+++ src/Modules/Discounts/Modules.Discounts.Application/Queries/Discounts/GetDiscountByCouponCode/GetDiscountByCouponCodeQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Modules.Discounts.Application.Dtos;
+
+namespace Modules.Discounts.Application.Queries.Discounts.GetDiscountByCouponCode
+{
+    public class GetDiscountByCouponCodeQuery : IRequest<DiscountDto>
+    {
+        public string CouponCode { get; set; }
+    }
+}
