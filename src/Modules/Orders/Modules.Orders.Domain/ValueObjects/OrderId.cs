63e9b8d3020230b6943a58decab2047845fddc49
mateuszbabski <babskimateusz@gmail.com>
2024-07-31 08:22:02 +0200

    order domain  value objects created

---
+++ src/Modules/Orders/Modules.Orders.Domain/ValueObjects/OrderId.cs
@@ -0,0 +1,22 @@
+﻿using Modules.Orders.Domain.Exceptions;
+
+namespace Modules.Orders.Domain.ValueObjects
+{
+    public record OrderId
+    {
+        public Guid Value { get; }
+
+        public OrderId(Guid value)
+        {
+            if (value == Guid.Empty)
+            {
+                throw new InvalidOrderIdException();
+            }
+
+            Value = value;
+        }
+
+        public static implicit operator Guid(OrderId id) => id.Value;
+        public static implicit operator OrderId(Guid value) => new(value);
+    }
+}
---
+++ src/Modules/Orders/Modules.Orders.Domain/ValueObjects/ReceiverId.cs
@@ -0,0 +1,22 @@
+﻿using Modules.Orders.Domain.Exceptions;
+
+namespace Modules.Orders.Domain.ValueObjects
+{
+    public record ReceiverId
+    {
+        public Guid Value { get; }
+
+        public ReceiverId(Guid value)
+        {
+            if (value == Guid.Empty)
+            {
+                throw new InvalidReceiverIdException();
+            }
+
+            Value = value;
+        }
+
+        public static implicit operator Guid(ReceiverId id) => id.Value;
+        public static implicit operator ReceiverId(Guid value) => new(value);
+    }
+}
