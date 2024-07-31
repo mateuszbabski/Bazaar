6d5896993aeacd4e205a74839923c522bf9de8e2
mateuszbabski <babskimateusz@gmail.com>
2024-07-31 08:20:59 +0200

    Order domain entities created

---
+++ src/Modules/Orders/Modules.Orders.Domain/Entities/OrderItem.cs
@@ -0,0 +1,48 @@
+﻿using Modules.Orders.Domain.ValueObjects;
+using Shared.Domain;
+using Shared.Domain.ValueObjects;
+
+namespace Modules.Orders.Domain.Entities
+{
+    public class OrderItem : Entity
+    {
+        public OrderItemId Id { get; private set; }
+        public Guid ProductId { get; private set; }
+        public Guid ShopId { get; private set; }
+        public OrderId OrderId { get; private set; }
+        public int Quantity { get; private set; } = 1;
+        public MoneyValue Price { get; private set; }
+
+        private OrderItem() { }
+
+        internal OrderItem(Guid productId,
+                           Guid shopId,
+                           OrderId orderId,
+                           int quantity,
+                           MoneyValue productPrice
+                           ) 
+        {
+            Id = new OrderItemId(Guid.NewGuid());
+            ProductId = productId;
+            OrderId = orderId;
+            ShopId = shopId;
+            Quantity = quantity;
+            Price = CountOrderItemPrice(quantity, productPrice.Amount, productPrice.Currency);
+        }
+
+        public static OrderItem CreateOrderItemFromProduct(
+                                                        Guid productId,
+                                                        Guid shopId,
+                                                        OrderId orderId,
+                                                        int quantity,
+                                                        MoneyValue productPrice)
+        {
+            return new OrderItem(productId, shopId, orderId, quantity, productPrice);
+        }
+
+        private static MoneyValue CountOrderItemPrice(int quantity, decimal price, string currency)
+        {
+            return MoneyValue.Of(quantity * price, currency);
+        }
+    }
+}
---
+++ src/Modules/Orders/Modules.Orders.Domain/Entities/OrderShippingMethod.cs
@@ -0,0 +1,31 @@
+﻿using MediatR.NotificationPublishers;
+using Shared.Domain;
+using Shared.Domain.ValueObjects;
+
+namespace Modules.Orders.Domain.Entities
+{
+    public class OrderShippingMethod : Entity
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public MoneyValue Price { get; private set; }
+
+        private OrderShippingMethod()
+        {
+        }
+
+        private OrderShippingMethod(Name name, MoneyValue price)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Price = price;
+        }
+
+        public static OrderShippingMethod CreateNewShippingMethod(string name, decimal amount, string currency)
+        {
+            var shippingMethod = new OrderShippingMethod(name, MoneyValue.Of(amount, currency));
+
+            return shippingMethod;
+        }
+    }
+}
