07dd549785867f622973af0a7034064cf6858266
mateuszbabski <babskimateusz@gmail.com>
2024-07-31 08:21:37 +0200

    Order repository created

---
+++ src/Modules/Orders/Modules.Orders.Domain/Repositories/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿using Modules.Orders.Domain.Entities;
+
+namespace Modules.Orders.Domain.Repositories
+{
+    public interface IOrderRepository
+    {
+        Task<Order> Add(Order order);
+    }
+}
---
+++ src/Modules/Orders/Modules.Orders.Infrastructure/Repository/OrderRepository.cs
@@ -0,0 +1,22 @@
+﻿using Modules.Orders.Domain.Entities;
+using Modules.Orders.Domain.Repositories;
+using Modules.Orders.Infrastructure.Context;
+
+namespace Modules.Orders.Infrastructure.Repository
+{
+    internal class OrderRepository : IOrderRepository
+    {
+        private readonly OrdersDbContext _dbContext;
+
+        public OrderRepository(OrdersDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<Order> Add(Order order)
+        {
+            await _dbContext.Orders.AddAsync(order);
+
+            return order;
+        }
+    }
+}
